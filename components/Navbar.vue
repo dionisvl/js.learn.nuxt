944c90262aef98d789ec9511f8e07d3f79040180
den <dden_vlad@mail.ru>
2020-03-19 00:14:10 +0300

    Подгрузка ссылок 23:30

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,17 +1,22 @@
 <template>
   <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <a class="navbar-brand" href="#">Nuxt SSR</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
 
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav mr-auto">
-        <li class="nav-item active">
-          <a class="nav-link" href="/">Home</a>
+        <li class="nav-item">
+          <nuxt-link exact active-class="active" class="nav-link" to="/">
+            Home
+          </nuxt-link>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="/about">About</a>
+          <nuxt-link active-class="active" class="nav-link" to="/about">
+            About
+          </nuxt-link>
         </li>
       </ul>
     </div>
